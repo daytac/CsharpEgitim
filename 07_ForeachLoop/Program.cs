43fe72f67884bb4d54684f5b0baaf5574d3f6a87
Doga Aytac <dogaamendes98@gmail.com>
2025-07-04 15:55:07 +0300

    Csharp kampi 2. gün

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region foreach döngüsü
+
+            //string[] cities = { "bursa", "ankara", "hatay", "uşak", "tokat" };
+            //foreach (string city in cities) 
+            //{ 
+            //    Console.WriteLine(city);
+            //}
+
+
+            //int[] number = { 1154, 2872, 545, 68, 265, 9988, 325 };
+            //int total = 0;
+            //foreach (int i in number)
+            //{
+            //    total += i;
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+            //Console.WriteLine("toplam:"+total);
+
+
+            //List<int> numbers = new List<int>()
+            //{ 1,2,3,4,5,6,7,8};
+            //foreach (int i in numbers)
+            //{ 
+            //    Console.WriteLine(i);
+            //}
+
+            //string word = "selam";
+            //foreach (char c in word) 
+            //{ 
+            //    Console.WriteLine(c);
+            //}
+
+            #endregion
+
+            #region örnek sınav sistemi
+
+            //öğrenci sayısını kullanıcıdan alma
+            Console.Write("öğrenci sayisi: ");
+            int studentCount=int.Parse(Console.ReadLine());
+
+            //öğrenci isimlerini ve not ortalamalarını saklicak diziler
+            string[] studentNames=new string[studentCount];
+            double[] studentExamAvg=new double[studentCount];   
+
+            for (int i=0; i<studentCount; i++)
+            {
+                Console.Write($"{i+1}. öğrencinin ismi:");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExemResult = 0;
+
+                //her öğrenci için 3 sınav girişi
+                for(int j=0; j<3; j++) 
+                {
+                    Console.Write($"{studentNames[i] } nin {j+1}. notu: ");
+                    double value=double.Parse(Console.ReadLine());
+                    totalExemResult += value;
+                }
+                studentExamAvg[i] = totalExemResult/3;    
+            }
+
+            //sınav ortalamaları
+            for(int i=0;i<studentCount;i++)
+            {
+                Console.WriteLine($"{studentNames[i]} nin ortalamasi: { studentExamAvg[i]}");
+                //ortalama ve geçip kalma durumu
+                if (studentExamAvg[i] >50) 
+                { 
+                    Console.WriteLine($"{studentNames[i]} dersi geçti"); 
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} dersten kaldi");
+                }
+            }
+
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ 08_Methods/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region void metotlar
+            //geriye değer döndürmeyen metotlar
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Doğa");
+            //    Console.WriteLine("Enis");
+            //}
+
+            //CustomerList();
+            #endregion
+
+            #region geriye değer döndürmeyen string parametreli metotlar
+
+            //void WriteMethod(string Customer)
+            //{
+            //    Console.WriteLine(Customer);    
+            //}
+            //WriteMethod("aaa");
+
+            #endregion
+
+            #region geriye değer döndürmeyen int parametreli metotlar
+
+            //void Sum(int a , int b, int c)
+            //{
+            //    int result = a + b + c;
+            //    Console.WriteLine(result);  
+            //}
+            //Sum(2, 155, 3);
+
+            #endregion
+
+            #region geriye değer döndüren metotler
+
+            //string StudentName()
+            //{
+            //    string name = "doğa";
+            //    string Surname = "aytac";
+            //    return name+ " " + Surname; 
+            //}
+            //Console.WriteLine(StudentName());
+
+            #endregion
+
+            #region geriye değer döndüren string parametreli metotlar
+
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+            //    string cardInfo = "-Ülke:" + countryName +" -Başkent:"+capital +" -Bayrak:" + flagColor;
+            //    return cardInfo;
+            //}
+
+            //string x, y, z;
+            //Console.Write("Ülke adi: ");
+            //x= Console.ReadLine();
+            //Console.Write("Başkent: ");
+            //y= Console.ReadLine();
+            //Console.Write("Bayrak: ");
+            //z= Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x, y, z));
+
+            #endregion
+
+            #region geriye değer döndüren int parametreli metotlar
+
+            //int Sum(int x, int y)
+            //{
+            //    int sonuc = x + y;
+            //    return sonuc;
+            //}
+            //Console.WriteLine(Sum(41,27));
+
+            #endregion
+
+            #region örnek
+            string ExemResult(string student, int e1,int e2, int e3)
+            {
+                int result = e1 + e2 + e3 / 3;
+                if (result > 50)
+                    return "geçti";
+                else return "kaldi";
+            }
+
+            Console.WriteLine(ExemResult("doa", 60, 70, 80));
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
